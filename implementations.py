68e4acf9d9e680874aed20091efab36af2047de2
Benedek Harsanyi <hben.0204@gmail.com>
2021-10-27 17:58:03 +0200

    add corr heatmap

--- implementations.py
+++ implementations.py
@@ -2,6 +2,7 @@
 """some helper functions for project 1."""
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 
 def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
     """
@@ -449,4 +450,37 @@ def reg_logistic_regression_newton(y, tx, lambda_, initial_w, max_iters, batch_s
     return w, loss
 
 
+"""VISUALIZATION"""
 
+def plot_correlation_heatmap():
+
+    features = ['DER_mass_MMC', 'DER_mass_transverse_met_lep', 'DER_mass_vis',
+       'DER_pt_h', 'DER_deltaeta_jet_jet', 'DER_mass_jet_jet',
+       'DER_prodeta_jet_jet', 'DER_deltar_tau_lep', 'DER_pt_tot', 'DER_sum_pt',
+       'DER_pt_ratio_lep_tau', 'DER_met_phi_centrality',
+       'DER_lep_eta_centrality', 'PRI_tau_pt', 'PRI_tau_eta', 'PRI_tau_phi',
+       'PRI_lep_pt', 'PRI_lep_eta', 'PRI_lep_phi', 'PRI_met', 'PRI_met_phi',
+       'PRI_met_sumet', 'PRI_jet_num', 'PRI_jet_leading_pt',
+       'PRI_jet_leading_eta', 'PRI_jet_leading_phi', 'PRI_jet_subleading_pt',
+       'PRI_jet_subleading_eta', 'PRI_jet_subleading_phi', 'PRI_jet_all_pt']
+
+    corr = np.corrcoef(tX, rowvar=False)
+
+
+    fig, ax = plt.subplots(figsize=(25,25))
+    im = ax.imshow(corr,  cmap='viridis')
+    ax.set_xticks(np.arange(30))
+    ax.set_yticks(np.arange(30))
+    ax.set_xticklabels(features)
+    ax.set_yticklabels(features)
+
+
+    plt.setp(ax.get_xticklabels(), rotation=90, ha="right")
+
+    for i in range(30):
+        for j in range(30):
+            text = ax.text(j, i, "{:.2f}".format(corr[i, j]),ha="center", va="center", color="w")
+
+    ax.set_title("Feature correlations")
+    fig.tight_layout()
+    plt.show()
